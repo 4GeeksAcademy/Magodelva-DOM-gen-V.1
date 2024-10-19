f103c8a7939585cb6618795795bc333d567cb4e6
Magodelva25 <migudz@gmail.com>
2024-10-19 14:42:37 +0200

    v.1 functional

--- src/app.js
+++ src/app.js
@@ -1,11 +1,37 @@
-/* eslint-disable */
-import "bootstrap";
-import "./style.css";
+// Recibimos los valores del formulario
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+document.getElementById('dataEntry').addEventListener('submit', function(event) {
+  event.preventDefault(); // Evita que el formulario se envíe
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+
+// Obtenemos los valores de los inputs
+
+
+  const input1 = document.getElementById('Primera palabra').value;
+  const input2 = document.getElementById('Segunda palabra').value;
+  const input3 = document.getElementById('Tercera palabra').value;
+  const domainExtensions = ['.com', '.es', '.io', '.org', '.dev']
+
+// Constante para aleatoreizar los inputs
+
+  const inputsToRandomize = [input1, input2, input3];
+// Establecemos la función para aleatorizar los valores 
+
+const inputsToRandomizeValue1 = Math.floor(Math.random() * 3);
+const inputsToRandomizeValue2 = Math.floor(Math.random() * 3);
+const inputsToRandomizeValue3 = Math.floor(Math.random() * 3);
+const domainExtensionsRandomized = Math.floor(Math.random() * 5);
+
+const returnGeneratedDomain = () =>{
+  return `www.${inputsToRandomize[inputsToRandomizeValue1]}${inputsToRandomize[inputsToRandomizeValue2]}${inputsToRandomize[inputsToRandomizeValue3]}${domainExtensions[domainExtensionsRandomized]}`
+}
+
+
+
+  // Concatenar los valores aleatorizados para formar el dominio
+  const domain = returnGeneratedDomain();  // Ejemplo usando ".com"
+
+  // Mostrar el resultado
+  document.getElementById('gen-dom').innerText = `Dominio generado: ${domain}`;
+});
+
