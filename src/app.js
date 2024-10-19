855571f50c3f6dcc3c8c12339412bb0f5dd84e4f
Manuel Gonzalez <mi4go@MacBook-Pro-de-Manuel.local>
2024-10-19 16:34:54 +0200

    V.1.1

--- src/app.js
+++ src/app.js
@@ -4,35 +4,41 @@ document.getElementById('dataEntry').addEventListener('submit', function(event) 
   event.preventDefault(); // Evita que el formulario se envíe
 
 
+// Formatear el string de entrada
+
+const caseFormat = (element) => { 
+  return element.toLowerCase()
+  };
+
+
 // Obtenemos los valores de los inputs
 
 
-  const input1 = document.getElementById('Primera palabra').value;
-  const input2 = document.getElementById('Segunda palabra').value;
-  const input3 = document.getElementById('Tercera palabra').value;
-  const domainExtensions = ['.com', '.es', '.io', '.org', '.dev']
+  const input1 = caseFormat(document.getElementById('Primera palabra').value);
+  const input2 = caseFormat(document.getElementById('Segunda palabra').value);
+  const input3 = caseFormat(document.getElementById('Tercera palabra').value);
+  const domains = ['.com', '.es', '.io', '.org', '.dev'];
 
-// Constante para aleatoreizar los inputs
+// Constante auxiliar para aleatorizar
 
   const inputsToRandomize = [input1, input2, input3];
 
-// Establecemos la función para aleatorizar los valores 
+// Establecemos las constantes randomizadas
 
-const inputsToRandomizeValue1 = Math.floor(Math.random() * 3); // Se añaden 3 iguales para dar 3 valores diferentes
-const inputsToRandomizeValue2 = Math.floor(Math.random() * 3);
-const inputsToRandomizeValue3 = Math.floor(Math.random() * 3);
-const domainExtensionsRandomized = Math.floor(Math.random() * 5);
+const first = Math.floor(Math.random() * 3); // Se añaden 3 iguales para dar 3 valores diferentes
+const second = Math.floor(Math.random() * 3);
+const third = Math.floor(Math.random() * 3);
+const domainRandom = Math.floor(Math.random() * 5);
 
+// 
 
   // Concatenar los valores aleatorizados para formar el dominio
-const returnGeneratedDomain = () =>{
-  return `www.${inputsToRandomize[inputsToRandomizeValue1]}${inputsToRandomize[inputsToRandomizeValue2]}${inputsToRandomize[inputsToRandomizeValue3]}${domainExtensions[domainExtensionsRandomized]}`
-}
+const unifyDomainItems =`www.${inputsToRandomize[first]}${inputsToRandomize[second]}${inputsToRandomize[third]}${domains[domainRandom]}`;
 
-const domain = returnGeneratedDomain();  // Ejemplo usando ".com"
+const domain = unifyDomainItems;  // Ejemplo usando ".com"
 
   // Mostrar el resultado
   
-  document.getElementById('gen-dom').innerText = `Dominio generado: ${domain}`;
+  document.getElementById('gen-dom').innerText = `${domain}`;
 });
 
