7dc99f7dd6dfee1067f3d8ee64f7cbe101ff7609
Magodelva25 <migudz@gmail.com>
2024-10-19 14:46:28 +0200

    js update

--- src/app.js
+++ src/app.js
@@ -15,23 +15,24 @@ document.getElementById('dataEntry').addEventListener('submit', function(event) 
 // Constante para aleatoreizar los inputs
 
   const inputsToRandomize = [input1, input2, input3];
+
 // Establecemos la función para aleatorizar los valores 
 
-const inputsToRandomizeValue1 = Math.floor(Math.random() * 3);
+const inputsToRandomizeValue1 = Math.floor(Math.random() * 3); // Se añaden 3 iguales para dar 3 valores diferentes
 const inputsToRandomizeValue2 = Math.floor(Math.random() * 3);
 const inputsToRandomizeValue3 = Math.floor(Math.random() * 3);
 const domainExtensionsRandomized = Math.floor(Math.random() * 5);
 
+
+  // Concatenar los valores aleatorizados para formar el dominio
 const returnGeneratedDomain = () =>{
   return `www.${inputsToRandomize[inputsToRandomizeValue1]}${inputsToRandomize[inputsToRandomizeValue2]}${inputsToRandomize[inputsToRandomizeValue3]}${domainExtensions[domainExtensionsRandomized]}`
 }
 
-
-
-  // Concatenar los valores aleatorizados para formar el dominio
-  const domain = returnGeneratedDomain();  // Ejemplo usando ".com"
+const domain = returnGeneratedDomain();  // Ejemplo usando ".com"
 
   // Mostrar el resultado
+  
   document.getElementById('gen-dom').innerText = `Dominio generado: ${domain}`;
 });
 
