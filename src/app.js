7fd5a2da575e12e8424520242921a32f0d99c933
Manuel Gonzalez <mi4go@MacBook-Pro-de-Manuel.local>
2024-10-22 13:12:35 +0200

    actualizando input data

--- src/app.js
+++ src/app.js
@@ -1,44 +1,41 @@
 // Recibimos los valores del formulario
 
-document.getElementById('data-entry').addEventListener('submit', function(event) {
+document.getElementById('data-entry').addEventListener('submit', function(event)  {
   event.preventDefault(); // Evita que el formulario se envíe
 
 
 // Formatear el string de entrada
 
 const caseFormat = (element) => { 
-  return element.toLowerCase().trim()
+  return element.toLowerCase()
   };
 
+  const inputOne = [caseFormat(document.getElementById('pronouns').value)];
+  const inputTwo = [caseFormat(document.getElementById('elements').value)];
+  const inputThree = [caseFormat(document.getElementById('adjective').value)];
+  const domains = ['.com', '.es', '.io', '.org', '.dev'];
 
 // Obtenemos los valores de los inputs
 
+const getArray = (element) => {
+  return element.map();
+};
 
-  const firstInput = [caseFormat(document.getElementById('first-word').value)];
-  const secondInput = [caseFormat(document.getElementById('second-word').value)];
-  const thirdInput = [caseFormat(document.getElementById('third-word').value)];
-  const domains = ['.com', '.es', '.io', '.org', '.dev'];
+const pronouns = getArray(inputOne);
+const elements = getArray(inputTwo);
+const adjectives = getArray(inputThree);
 
-// Constante auxiliar para aleatorizar
 
-  const inputsToRandomize = [firstInput, secondInput, thirdInput];
 
-// Establecemos las constantes randomizadas
+// Convertir arr
 
-const first = Math.floor(Math.random() * 3); // Se añaden 3 iguales para dar 3 valores diferentes
-const second = Math.floor(Math.random() * 3);
-const third = Math.floor(Math.random() * 3);
-const domainRandom = Math.floor(Math.random() * 5);
-
-// 
-
-  // Concatenar los valores aleatorizados para formar el dominio
-const unifyDomainItems =`www.${inputsToRandomize[first]}${inputsToRandomize[second]}${inputsToRandomize[third]}${domains[domainRandom]}`;
-
-const domain = unifyDomainItems;  // Ejemplo usando ".com"
-
-  // Mostrar el resultado
-  
-  console.log(document.getElementById('gen-dom').innerText = `${domain}`);
-});
-
+for(let pronoun of pronouns) {
+  for(let elemento of elements) {
+    for(let adjetivo of adjectives) {
+      for(let domain of domains) {
+        console.log([pronoun, elemento, adjetivo, domain]);
+      }
+    }
+  }
+}
+})
